520b6719ef12e419381c98f4f662937132c7cfe5
SuJiKiNen <luishen@vip.qq.com>
2019-05-19 23:52:41 +0800

    Move unit test to a directory

--- tests/unit/test_functions.py
+++ tests/unit/test_functions.py
@@ -12,18 +12,22 @@ from src.pyfiledir import (
 )
 
 
-@pytest.mark.parametrize("path,excepted", [
-    ("/test1", ("/test1", slice(None, None, 1))),
-    ("/测试1", ("/测试", slice(0, 1, 1))),
-])
+@pytest.mark.parametrize(
+    "path,excepted", [
+        ("/test1", ("/test1", slice(None, None, 1))),
+        ("/测试1", ("/测试", slice(0, 1, 1))),
+    ],
+)
 def test_rsplit_selection(path, excepted):
     assert rsplit_selection(path) == excepted
 
 
-@pytest.mark.parametrize("path,excepted", [
-    ("/test1", True),
-    ("/测试1", False),
-])
+@pytest.mark.parametrize(
+    "path,excepted", [
+        ("/test1", True),
+        ("/测试1", False),
+    ],
+)
 def test_isascii(path, excepted):
     assert all_ascii(path) == excepted
 
@@ -45,18 +49,20 @@ def test_get_py(char, excepted):
     assert get_py(char) == excepted
 
 
-@pytest.mark.parametrize("env_val,excepted", [
-    ("1", True),
-    ("0", False),
-    ("yes", True),
-    ("no", False),
-    ("true", True),
-    ("false", False),
-    ("True", True),
-    ("False", False),
-    ("on", True),
-    ("off", False),
-])
+@pytest.mark.parametrize(
+    "env_val,excepted", [
+        ("1", True),
+        ("0", False),
+        ("yes", True),
+        ("no", False),
+        ("true", True),
+        ("false", False),
+        ("True", True),
+        ("False", False),
+        ("on", True),
+        ("off", False),
+    ],
+)
 def test_get_truthy_env(env_val, excepted):
     key = 'PYFILEDIR_ADD_TRAILING_SLASH'
     os.environ[key] = env_val
@@ -68,10 +74,12 @@ def test_do_py_completion_on_not_exists_path():
         do_py_completion("/not_exists_path/file")
 
 
-@pytest.mark.parametrize("files,typed", [
-    (["abc"], "abc"),
-    (["arise", "are"], "a"),
-])
+@pytest.mark.parametrize(
+    "files,typed", [
+        (["abc"], "abc"),
+        (["arise", "are"], "a"),
+    ],
+)
 def test_main_function(fs, test_home_dir, files, typed, capsys):
     os.chdir(test_home_dir)
     [fs.create_file(f) for f in files]
--- tests/unit/test_matches.py
+++ tests/unit/test_matches.py
@@ -50,11 +50,13 @@ def test_complete_implicit_current_directory(fs, test_home_dir):
     assert do_py_completion("t") == SEP.join(["test"])
 
 
-@pytest.mark.parametrize("dirs,files,typed,excepted", [
-    (["subdir1", "subdir2"], ["file1", "file2"], "~/", unicode_sort(["subdir1", "subdir2", "file1", "file2"])),
-    (["subdir1", "subdir2"], [], "~/", unicode_sort(["subdir1", "subdir2"])),
-    ([], ["file1", "file2"], "~/", unicode_sort(["file1", "file2"])),
-])
+@pytest.mark.parametrize(
+    "dirs,files,typed,excepted", [
+        (["subdir1", "subdir2"], ["file1", "file2"], "~/", unicode_sort(["subdir1", "subdir2", "file1", "file2"])),
+        (["subdir1", "subdir2"], [], "~/", unicode_sort(["subdir1", "subdir2"])),
+        ([], ["file1", "file2"], "~/", unicode_sort(["file1", "file2"])),
+    ],
+)
 def test_empty_basename_match_all_files_or_dirs_in_directory(dirs, files, typed, excepted, fs, test_home_dir):
     SEP = get_env("PYFILEDIR_CANDIDATE_SEP")
     for d in dirs:
@@ -65,46 +67,50 @@ def test_empty_basename_match_all_files_or_dirs_in_directory(dirs, files, typed,
     assert do_py_completion(typed) == SEP.join(excepted)
 
 
-@pytest.mark.parametrize("dirs,typed,excepted", [
-    (
-        ["/1024", "/1025"],
-        "/1",
-        ["/1024", "/1025"],
-    ),
-    (
-        ["/2021_12_28", "/2023_10_15", "/2025_04_12"],
-        "/2023",
-        ["/2023_10_15"],
-    ),
-    (
-        ["/个人图片", "/个人信息"],
-        "/个" + "1",
-        [unicode_sort(["/个人图片", "/个人信息"])[0]],
-    ),
-])
+@pytest.mark.parametrize(
+    "dirs,typed,excepted", [
+        (
+            ["/1024", "/1025"],
+            "/1",
+            ["/1024", "/1025"],
+        ),
+        (
+            ["/2021_12_28", "/2023_10_15", "/2025_04_12"],
+            "/2023",
+            ["/2023_10_15"],
+        ),
+        (
+            ["/个人图片", "/个人信息"],
+            "/个" + "1",
+            [unicode_sort(["/个人图片", "/个人信息"])[0]],
+        ),
+    ],
+)
 def test_number_selection(dirs, typed, excepted, fs):
     SEP = get_env("PYFILEDIR_CANDIDATE_SEP")
     [fs.create_dir(d) for d in dirs]
     assert do_py_completion(typed) == SEP.join(excepted)
 
 
-@pytest.mark.parametrize("dirs,typed,excepted", [
-    (
-        ["第{}组".format(i) for i in range(1, 9)],
-        "d1",
-        ["第1组"],
-    ),
-    (
-        ["第{}组".format(i) for i in range(1, 9)],
-        "第1",
-        ["第1组"],
-    ),
-    (
-        ["第{}组".format(i) for i in range(1, 20)],
-        "第11",
-        ["第10组"],
-    ),
-])
+@pytest.mark.parametrize(
+    "dirs,typed,excepted", [
+        (
+            ["第{}组".format(i) for i in range(1, 9)],
+            "d1",
+            ["第1组"],
+        ),
+        (
+            ["第{}组".format(i) for i in range(1, 9)],
+            "第1",
+            ["第1组"],
+        ),
+        (
+            ["第{}组".format(i) for i in range(1, 20)],
+            "第11",
+            ["第10组"],
+        ),
+    ],
+)
 def test_number_selection_start_from_one(dirs, typed, excepted, fs):
     SEP = get_env("PYFILEDIR_CANDIDATE_SEP")
     [fs.create_dir(d) for d in dirs]
@@ -123,51 +129,59 @@ def test_solo_completion_on_files_do_completion_again_not_add_forward_slash(fs):
     assert do_py_completion(do_py_completion("/te")) == "/test_file"
 
 
-@pytest.mark.parametrize("cn_char,alpha", [
-    ("重", "c"),
-    ("重", "z"),
-    ("乐", "l"),
-    ("乐", "y"),
-])
+@pytest.mark.parametrize(
+    "cn_char,alpha", [
+        ("重", "c"),
+        ("重", "z"),
+        ("乐", "l"),
+        ("乐", "y"),
+    ],
+)
 def test_do_polyphone_match(cn_char, alpha):
     assert do_polyphone_match(cn_char, alpha)
 
 
-@pytest.mark.parametrize("dirs,typed,excepted", [
-    (["/乐趣"], "/lq", ["/乐趣"]),
-    (["/音乐"], "/yy", ["/音乐"]),
-    (["/重要"], "/zy", ["/重要"]),
-    (["/重复"], "/cf", ["/重复"]),
-])
+@pytest.mark.parametrize(
+    "dirs,typed,excepted", [
+        (["/乐趣"], "/lq", ["/乐趣"]),
+        (["/音乐"], "/yy", ["/音乐"]),
+        (["/重要"], "/zy", ["/重要"]),
+        (["/重复"], "/cf", ["/重复"]),
+    ],
+)
 def test_polyphone_completion_mtach(dirs, typed, excepted, fs):
     SEP = get_env("PYFILEDIR_CANDIDATE_SEP")
     [fs.create_dir(d) for d in dirs]
     assert do_py_completion(typed) == SEP.join(excepted)
 
 
-@pytest.mark.parametrize("dirs,typed,excepted", [
-    (["/芙蓉", "/fr"], "/##", ["/芙蓉"]),
-    (["/目录"], "/##", ["/目录"]),
-])
+@pytest.mark.parametrize(
+    "dirs,typed,excepted", [
+        (["/芙蓉", "/fr"], "/##", ["/芙蓉"]),
+        (["/目录"], "/##", ["/目录"]),
+    ],
+)
 def test_number_sign_is_unicode_wildcard(dirs, typed, excepted, fs):
     SEP = get_env("PYFILEDIR_CANDIDATE_SEP")
     [fs.create_dir(d) for d in dirs]
     assert do_py_completion(typed) == SEP.join(excepted)
 
 
-@pytest.mark.parametrize("dirs,typed,excepted", [
-    (["/test1", "/test2"], "/te", ["/test"]),
-    (["/学习java", "/学习javascript"], "/x", ["/学习java"]),
-    (
-        [
-            "/学习java",
-            "/学习javascript",
-            "/xx"
-        ],
-        "/x",
-        unicode_sort(["/学习java", "/学习javascript", "/xx"]),
-    ),
-])
+@pytest.mark.parametrize(
+    "dirs,typed,excepted", [
+        (["/test1", "/test2"], "/te", ["/test"]),
+        (["/学习java", "/学习javascript"], "/x", ["/学习java"]),
+        (
+            [
+                "/学习java",
+                "/学习javascript",
+                "/xx",
+            ],
+            "/x",
+            unicode_sort(["/学习java", "/学习javascript", "/xx"]),
+        ),
+    ],
+)
 @patch.dict(
     os.environ,
     {
@@ -180,9 +194,11 @@ def test_complete_common_prefix_first(dirs, typed, excepted, fs):
     assert do_py_completion(typed) == SEP.join(excepted)
 
 
-@pytest.mark.parametrize("dirs,typed,excepted", [
-    (["/Test1", "/TEST2"], "/te", unicode_sort(["/Test1", "/TEST2"])),
-])
+@pytest.mark.parametrize(
+    "dirs,typed,excepted", [
+        (["/Test1", "/TEST2"], "/te", unicode_sort(["/Test1", "/TEST2"])),
+    ],
+)
 @patch.dict(
     os.environ,
     {
